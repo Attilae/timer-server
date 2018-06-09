717aa854763273a9e6ee0b157a6074f8cd289e49
Attilae <attila.erdei87@gmail.com>
2018-06-10 00:42:24 +0200

    init

---
+++ src/main/java/hu/erdeiattila/poll/model/RoleName.java
@@ -0,0 +1,6 @@
+package hu.erdeiattila.poll.model;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/hu/erdeiattila/poll/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package hu.erdeiattila.poll.repository;
+
+import hu.erdeiattila.poll.model.Role;
+import hu.erdeiattila.poll.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
